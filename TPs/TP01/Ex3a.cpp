eb291d730ff558f6842b5c2c2fe74b8447fe35d2
pascal-mignot-urca <36710577+pascal-mignot-urca@users.noreply.github.com>
2021-01-11 08:37:06 +0100

    Update Ex3a.cpp

--- TPs/TP01/Ex3a.cpp
+++ TPs/TP01/Ex3a.cpp
@@ -1,14 +1,21 @@
 #include <cstdio>
 #include <utility>
+
+// But: comprendre quels sont les différentes déclarations possibles et les différentes combinaisons d'appel.
+// A savoir: 
+// 1) décommenter une fonction, puis regarder quels sont les appels qui fonctionnent (le code ne devrait pas compiler, mettre en commentaire les appels
+//    qui provoquent des erreurs de compilation). Recommencer pour la fonction suivante.
+// 2) essayer de décommenter deux fonctions, et voir quelles celles qui sont compatibles, et les appels qu'elles prennent en charge.
+// vous devriez retrouver les résultats de l'exercice 3 du TD (voir aussi page 29 du polycopié).
  
 int fun(int  x)             { printf("call fun(int x)\n"); return x; }
-int fun(const int  x)       { printf("call fun(const int x)\n"); return x; }
+//int fun(const int  x)       { printf("call fun(const int x)\n"); return x; }
 
-int fun(int &x)             { printf("call fun(int &x)\n"); return x; }
-int fun(const int &x)       { printf("call fun(const int &x)\n"); return x; }
+//int fun(int &x)             { printf("call fun(int &x)\n"); return x; }
+//int fun(const int &x)       { printf("call fun(const int &x)\n"); return x; }
 
-int fun(int &&x)            { printf("call fun(int &&x)\n"); return x; }
-int fun(const int &&x)      { printf("call fun(const int &&x)\n"); return x; }
+//int fun(int &&x)            { printf("call fun(int &&x)\n"); return x; }
+//int fun(const int &&x)      { printf("call fun(const int &&x)\n"); return x; }
 
 int main() {
 	int  		a=3, &b=a;
