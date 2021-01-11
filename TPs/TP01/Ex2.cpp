617b5933353d74dd3a22f5ecac5a5497f8d174db
pascal-mignot-urca <36710577+pascal-mignot-urca@users.noreply.github.com>
2021-01-11 08:38:44 +0100

    Update Ex2.cpp

--- TPs/TP01/Ex2.cpp
+++ TPs/TP01/Ex2.cpp
@@ -1,7 +1,7 @@
 #include <cstdio>
 #include <iostream>
 
-// Les fonctions sont écrites pour utiliser
+// Les fonctions sont écrites pour utiliser 
 // les spécificités du type de retour
 int    		fun3a(int x)  { return x; }
 int&		fun3b(int &x) { return x; }
@@ -17,6 +17,87 @@ const int* const fun3D(int *x) { return x; }
 
 int main() {
 	int  a = 1;
+	
+	{
+		int b1 = fun3a(a);
+		int b2 = fun3b(a);
+		int b3 = fun3c(a);
+		int b4 = fun3d();
+		int b5 = fun3e(a);
+		int b6 = fun3f(a);
+	}
 
-		const int &&b3 = fun3c(a);	
+	{
+		int &b1 = fun3a(a);
+		int &b2 = fun3b(a);
+		int &b3 = fun3c(a);
+		int &b4 = fun3d();
+		int &b5 = fun3e(a);
+		int &b6 = fun3f(a);
+	}
+
+	{
+		int &&b1 = fun3a(a);
+		int &&b2 = fun3b(a);
+		int &&b3 = fun3c(a);
+		int &&b4 = fun3d();
+		int &&b5 = fun3e(a);
+		int &&b6 = fun3f(a);
+	}
+
+	{
+		const int b1 = fun3a(a);
+		const int b2 = fun3b(a);
+		const int b3 = fun3c(a);
+		const int b4 = fun3d();
+		const int b5 = fun3e(a);
+		const int b6 = fun3f(a);
+	}
+
+	{
+		const int &b1 = fun3a(a);
+		const int &b2 = fun3b(a);
+		const int &b3 = fun3c(a);
+		const int &b4 = fun3d();
+		const int &b5 = fun3e(a);
+		const int &b6 = fun3f(a);
+	}
+
+	{
+		const int &&b1 = fun3a(a);
+		const int &&b2 = fun3b(a);
+		const int &&b3 = fun3c(a);
+		const int &&b4 = fun3d();
+		const int &&b5 = fun3e(a);
+		const int &&b6 = fun3f(a);
+	}
+	
+	{
+		int *b1 = fun3A(&a);
+		int *b2 = fun3B(&a);
+		int *b3 = fun3C(&a);
+		int *b4 = fun3D(&a);
+	}
+
+	{
+		const int *b1 = fun3A(&a);
+		const int *b2 = fun3B(&a);
+		const int *b3 = fun3C(&a);
+		const int *b4 = fun3D(&a);
+	}
+
+	{
+		int const *b1 = fun3A(&a);
+		int const *b2 = fun3B(&a);
+		int const *b3 = fun3C(&a);
+		int const *b4 = fun3D(&a);
+	}
+
+	{
+		const int* const b1 = fun3A(&a);
+		const int* const b2 = fun3B(&a);
+		const int* const b3 = fun3C(&a);
+		const int* const b4 = fun3D(&a);
+	}
+	
 }
